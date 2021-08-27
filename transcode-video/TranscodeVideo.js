316488ae0a7dfef7bd03298d09364104225b307f
hahu <atoshiki515@icloud.com>
2021-08-27 22:59:36 +0900

    Add files via upload

---
+++ transcode-video/TranscodeVideo.js
@@ -0,0 +1,46 @@
+'use strict';
+var AWS = require('aws-sdk');
+
+var elasticTranscoder = new AWS.ElasticTranscoder({
+    region: 'us-east-1'
+});
+
+exports.handler = function(event, context, callback){
+    
+
+    var key = event.Records[0].s3.object.key;
+
+    //the input file may have spaces so replace them with '+'
+    var sourceKey = decodeURIComponent(key.replace(/\+/g, ' '));
+
+    //remove the extension
+    var outputKey = sourceKey.split('.')[0];
+
+    console.log('key',key,sourceKey.outputkey);
+
+    var params = {
+        PipelineId: '1628849181473-4te305',
+        Input: {
+            Key: sourceKey
+        },
+        Outputs: [
+            {
+                Key: outputKey + '-1080p' + '.mp4',
+                PresetId: '1351620000001-000001' //Generic 1080p
+            },
+            {
+                Key: outputKey + '-720p' + '.mp4',
+                PresetId: '1351620000001-000010' //Generic 720p
+            },
+            {
+                Key: outputKey + '-web-720p' + '.mp4',
+                PresetId: '1351620000001-100070' //Web Friendly 720p
+            }
+        ]};
+
+    elasticTranscoder.createJob(params, function(error, data){
+        if (error){
+            callback(error);
+        }
+    });
+};
